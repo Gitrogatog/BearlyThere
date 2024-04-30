1d868902dd1e7ecc419fce218720c83ceb5e06b7
angsoenW <91903037+angsoenW@users.noreply.github.com>
2024-04-30 15:10:28 -0700

    minimap

--- PlayerCharacter.cs
+++ PlayerCharacter.cs
@@ -3,13 +3,13 @@ using System;
 
 public partial class PlayerCharacter : CharacterBody2D
 {
-    [Export] float moveSpeed;
-    public override void _Process(double delta)
-    {
-        float horizontalMove = Input.GetAxis("move_left", "move_right");
-        float verticalMove = Input.GetAxis("move_up", "move_down");
-        Vector2 movement = new Vector2(horizontalMove, verticalMove).Normalized();
-        Velocity = moveSpeed * movement;
-        MoveAndSlide();
-    }
+	[Export] float moveSpeed;
+	public override void _Process(double delta)
+	{
+		float horizontalMove = Input.GetAxis("move_left", "move_right");
+		float verticalMove = Input.GetAxis("move_up", "move_down");
+		Vector2 movement = new Vector2(horizontalMove, verticalMove).Normalized();
+		Velocity = moveSpeed * movement;
+		MoveAndSlide();
+	}
 }
