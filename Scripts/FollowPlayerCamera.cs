1ef363061b2ec5dc5c7c68bf3ba7890d25fc7830
Gitrogatog <44677055+Gitrogatog@users.noreply.github.com>
2024-04-30 17:40:50 -0700

    Added camera that follows player

---
+++ Scripts/FollowPlayerCamera.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class FollowPlayerCamera : Camera2D
+{
+    [Export] Node2D playerNode;
+    public override void _Process(double delta)
+    {
+        Offset = playerNode.GlobalPosition;
+    }
+}
