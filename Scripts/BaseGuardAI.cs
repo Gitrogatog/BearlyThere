f92bb580d09cf74c74f68822ed3a9a345a7042d3
Gitrogatog <44677055+Gitrogatog@users.noreply.github.com>
2024-05-14 17:21:03 -0700

    Added sprites to guards

--- Scripts/BaseGuardAI.cs
+++ Scripts/BaseGuardAI.cs
@@ -5,11 +5,13 @@ public abstract partial class BaseGuardAI : Node2D
 {
     public GuardSightAI guardSightAI;
     public bool guardEnabled = true;
+    AnimatedSprite2D spriteNode;
 
     public void Init()
     {
         var globalEvents = GetNode<GlobalEvents>("/root/GlobalEvents");
         globalEvents.StartLevel += OnStartLevel;
+        spriteNode = GetNode<AnimatedSprite2D>("Sprite2D");
         guardSightAI = GetNode<GuardSightAI>("GuardSightAI");
         guardSightAI.MaxDetection += DisableGuard;
     }
@@ -41,4 +43,16 @@ public abstract partial class BaseGuardAI : Node2D
     }
 
     abstract public void MoveGuard(float delta);
+    // public void SetAnim(float rotation)
+    // {
+    //     float rot = SnapAngle(rotation);
+    //     if (rot == )
+    //         spriteNode.Play("idle_right");
+
+    // }
+    // float SnapAngle(float rotationToSnap)
+    // {
+    //     return (float)(Math.PI * 0.25 * Math.Round(rotationToSnap / Math.PI * 0.25f));
+
+    // }
 }
